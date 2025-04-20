d9f1a0f00c83d0d9bc130b6326341d6ce5e2b4c7
Phuc Nguyen <nguyenhuuphuc22052004@gmail.com>
2025-04-20 19:18:11 +0700

    fix(conversation): fix asyncHandler import in conversation

--- api/src/routes/message.route.js
+++ api/src/routes/message.route.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const messageController = require("../controllers/message.controller");
-const asyncHandler = require("../middlewares/asynHandler.middleware");
+const asyncHandler = require("../middlewares/asyncHandler.middleware");
 const router = express.Router();
 
 router.post("/add", asyncHandler(messageController.add));
